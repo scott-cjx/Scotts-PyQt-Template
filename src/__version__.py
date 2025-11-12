be1fa9592926e2efbf653bf679f2cbe25a90314b
Scottie C <48780312+scott-cjx@users.noreply.github.com>
2025-11-12 11:52:05 -0500

    fixed versioning

--- src/__version__.py
+++ src/__version__.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev6+g3f1b4489f.d20251111'
-__version_tuple__ = version_tuple = (0, 1, 'dev6', 'g3f1b4489f.d20251111')
+__version__ = version = '0.1.dev9+g58a62b2ab.d20251112'
+__version_tuple__ = version_tuple = (0, 1, 'dev9', 'g58a62b2ab.d20251112')
 
-__commit_id__ = commit_id = 'g3f1b4489f'
+__commit_id__ = commit_id = 'g58a62b2ab'
